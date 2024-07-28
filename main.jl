0b05256bb010e0f4b4c4308e98c2f7281dc5eb4a
JohnLucid1 <SobolII21@st.ithub.ru>
2024-07-28 23:43:25 +0300

    Added first tries

--- main.jl
+++ main.jl
@@ -1,10 +1,31 @@
-import HTTP
+import HTTP, EzXML
 #= 
 Every 10 minutes, it should go through a list of domains, check every path (maybe read sitemap) 
 If anywhere it founds an error code, the telegram bot will message me about it in the format 
 >> ERROR 404 on: chinadea.ru/cars/manjaro
 =#
 
-r = HTTP.request("GET", "http://httpbin.org/ip")
-println(r.status)
-println(String(r.body))
+
+const ten_minutes_in_secs = 600
+domains = [
+    "https://chinadeal.ru",
+    "https://autosalon-samara63.ru"
+]
+
+for domain in domains
+    r = HTTP.request("GET", domain)
+    status = r.status
+
+    if status != 200
+        # println("RESPONSE FROM $domain: $status")
+        # TODO: Message on telegram bot
+    end
+end
+
+function check_domain(domain::String)::Int
+    r = HTTP.request("GET", domain)
+end
+
+# function get_sitemap(domain::String)
+
+# sleep(ten_minutes_in_secs)
